eae0942013f226b4a4bff034530dc0d2cec50a95
vishalgddam873 <vishal18@navgurukul.org>
2023-05-24 10:32:56 +0530

    Initial comment

---
+++ TA2_2/main.py
@@ -0,0 +1,28 @@
+# Importing Library
+import numpy as np
+import cv2
+
+# Define the input and output paths
+
+
+# Load the color image
+
+
+# ------------Convert image to Grayscale --------------
+
+# Convert the color image to grayscale image
+
+
+# Save the color image to disk
+# Uncomment the below lines
+
+# outputPath = 'converted/grayScale.png'
+# cv2.imwrite(outputPath, grayscaleImage)
+
+
+# Display the converted image
+
+
+# Display a message indicating that the image has been saved
+# Uncomment the below lines
+# print('Converted Grayscale image saved to disk : ' + outputPath)
